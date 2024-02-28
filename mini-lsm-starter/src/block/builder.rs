d3319a1ef08604a854da36af78946a6ca49b7674
acking-sam <295329251@qq.com>
2024-02-28 05:22:48 +0000

    finish and pass w1 d3

--- mini-lsm-starter/src/block/builder.rs
+++ mini-lsm-starter/src/block/builder.rs
@@ -1,9 +1,9 @@
 #![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
+use super::{Block, SIZEOF_U16};
 use crate::key::{KeySlice, KeyVec};
-
-use super::Block;
+use bytes::BufMut;
 
 /// Builds a block.
 pub struct BlockBuilder {
@@ -20,22 +20,44 @@ pub struct BlockBuilder {
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        BlockBuilder {
+            offsets: Vec::new(),
+            data: Vec::new(),
+            block_size: block_size,
+            first_key: KeyVec::new(),
+        }
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: KeySlice, value: &[u8]) -> bool {
-        unimplemented!()
+        if !self.first_key.is_empty()
+            && self.data.len() + self.offsets.len() * 2 + key.len() + value.len() + SIZEOF_U16 * 3
+                > self.block_size
+        {
+            return false;
+        }
+        self.offsets.push(self.data.len() as u16);
+        self.data.put_u16(key.len() as u16);
+        self.data.put(&key.raw_ref()[..]);
+        self.data.put_u16(value.len() as u16);
+        self.data.put(value);
+        if self.first_key.is_empty() {
+            self.first_key = key.to_key_vec();
+        }
+        true
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.offsets.is_empty()
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        Block {
+            data: self.data,
+            offsets: self.offsets,
+        }
     }
 }
--- mini-lsm-starter/src/block/iterator.rs
+++ mini-lsm-starter/src/block/iterator.rs
@@ -4,8 +4,9 @@
 use std::sync::Arc;
 
 use crate::key::{KeySlice, KeyVec};
-
+pub(crate) const SIZEOF_U16: usize = std::mem::size_of::<u16>();
 use super::Block;
+use bytes::Buf;
 
 /// Iterates on a block.
 pub struct BlockIterator {
@@ -34,44 +35,113 @@ impl BlockIterator {
 
     /// Creates a block iterator and seek to the first entry.
     pub fn create_and_seek_to_first(block: Arc<Block>) -> Self {
-        unimplemented!()
+        let mut res = BlockIterator::new(block);
+        res.seek_to_first();
+        res
     }
 
     /// Creates a block iterator and seek to the first key that >= `key`.
     pub fn create_and_seek_to_key(block: Arc<Block>, key: KeySlice) -> Self {
-        unimplemented!()
+        let mut res = BlockIterator::new(block);
+        res.seek_to_key(key);
+        res
     }
 
     /// Returns the key of the current entry.
     pub fn key(&self) -> KeySlice {
-        unimplemented!()
+        self.key.as_key_slice()
     }
 
     /// Returns the value of the current entry.
     pub fn value(&self) -> &[u8] {
-        unimplemented!()
+        &self.block.data[self.value_range.0..self.value_range.1]
     }
 
     /// Returns true if the iterator is valid.
     /// Note: You may want to make use of `key`
     pub fn is_valid(&self) -> bool {
-        unimplemented!()
+        !self.key.is_empty()
     }
 
     /// Seeks to the first key in the block.
     pub fn seek_to_first(&mut self) {
-        unimplemented!()
+        self.idx = 0;
+        let entry_offsets_len = self.block.offsets.len();
+        if self.idx < entry_offsets_len {
+            let off = self.block.offsets[self.idx] as usize;
+            let mut entry = &self.block.data[off..];
+            let key_len = entry.get_u16() as usize;
+            let key = &entry[..key_len];
+            self.key.clear();
+            self.key.append(key);
+            entry.advance(key_len);
+            let val_len = entry.get_u16() as usize;
+            let value_offset_begin = off + key_len + SIZEOF_U16 * 2;
+            let value_offset_end = value_offset_begin + val_len;
+            self.value_range = (value_offset_begin, value_offset_end);
+            entry.advance(val_len);
+        } else {
+            self.key.clear();
+            self.value_range = (0, 0);
+        }
     }
 
     /// Move to the next key in the block.
     pub fn next(&mut self) {
-        unimplemented!()
+        let entry_offsets_len = self.block.offsets.len();
+        self.idx += 1;
+        if self.idx >= entry_offsets_len {
+            self.key.clear();
+            self.value_range = (0, 0);
+            return;
+        }
+        let off = self.block.offsets[self.idx] as usize;
+        let mut entry = &self.block.data[off..];
+        let key_len = entry.get_u16() as usize;
+        let key = &entry[..key_len];
+        self.key.clear();
+        self.key.append(key);
+        entry.advance(key_len);
+        let val_len = entry.get_u16() as usize;
+        let value_offset_begin = off + key_len + SIZEOF_U16 * 2;
+        let value_offset_end = value_offset_begin + val_len;
+        self.value_range = (value_offset_begin, value_offset_end);
+        entry.advance(val_len);
     }
 
     /// Seek to the first key that >= `key`.
     /// Note: You should assume the key-value pairs in the block are sorted when being added by
     /// callers.
-    pub fn seek_to_key(&mut self, key: KeySlice) {
-        unimplemented!()
+    pub fn seek_to_key(&mut self, _key: KeySlice) {
+        let entry_offsets_len = self.block.offsets.len();
+        self.idx = 0;
+        while self.idx < entry_offsets_len {
+            let off = self.block.offsets[self.idx] as usize;
+            let mut entry = &self.block.data[off..];
+            let key_len = entry.get_u16() as usize;
+            let key = &entry[..key_len];
+            self.key.clear();
+            self.key.append(key);
+            entry.advance(key_len);
+            let val_len = entry.get_u16() as usize;
+            let value_offset_begin = off + key_len + SIZEOF_U16 * 2;
+            let value_offset_end = value_offset_begin + val_len;
+            self.value_range = (value_offset_begin, value_offset_end);
+            entry.advance(val_len);
+            match self.key().cmp(&_key) {
+                std::cmp::Ordering::Less => {
+                    self.idx += 1;
+                    continue;
+                }
+                std::cmp::Ordering::Greater => {
+                    return;
+                }
+                std::cmp::Ordering::Equal => {
+                    return;
+                }
+            }
+        }
+        self.key.clear();
+        self.value_range = (0, 0);
     }
 }
